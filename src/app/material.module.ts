33f97b60fec08407a7ee582303bd04ba63ffa68c
fmohamed <fmohamed@F.M>
2020-02-25 00:53:35 +0200

    Add MatTableModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTableModule } from "@angular/material/table";
 @NgModule({
-  imports: [MatToolbarModule],
-  exports: [MatToolbarModule]
+  imports: [MatToolbarModule, MatTableModule],
+  exports: [MatToolbarModule, MatTableModule]
 })
 export class MaterialModule {}
