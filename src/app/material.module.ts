2a4f1048169465f4243869fa4acf9011177de13c
fmohamed <fmohamed@F.M>
2020-02-25 01:00:19 +0200

    Add MatPaginatorModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,8 +3,22 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTableModule } from "@angular/material/table";
 import { MatButtonModule } from "@angular/material/button";
 import { MatSortModule } from "@angular/material/sort";
+import { MatPaginatorModule } from "@angular/material/paginator";
+
 @NgModule({
-  imports: [MatToolbarModule, MatTableModule, MatButtonModule, MatSortModule],
-  exports: [MatToolbarModule, MatTableModule, MatButtonModule, MatSortModule]
+  imports: [
+    MatToolbarModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSortModule,
+    MatPaginatorModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSortModule,
+    MatPaginatorModule
+  ]
 })
 export class MaterialModule {}
