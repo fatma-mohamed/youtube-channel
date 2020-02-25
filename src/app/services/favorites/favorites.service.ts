464fa6d893dcc9bc9879bb658357301b8b675342
fmohamed <fmohamed@F.M>
2020-02-25 16:29:19 +0200

    Create Favorites service

---
+++ src/app/services/favorites/favorites.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { LocalstorageService } from "../localstorage/localstorage.service";
+import { KEY_FAV } from "src/app/models/constants";
+import { truncate } from "fs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class FavoritesService {
+  constructor(private localStorage: LocalstorageService) {}
+
+  async isFavorite(videoId) {
+    const favs = (await this.localStorage.get(KEY_FAV)) as string[];
+    if (!favs || favs.length == 0) {
+      return false;
+    }
+    if (favs.includes(videoId)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async addFav(videoId) {
+    return this.localStorage.appendToList(KEY_FAV, videoId);
+  }
+
+  async removeFav(videoId) {
+    return this.localStorage.removeFromList(KEY_FAV, videoId);
+  }
+}
