5e3b0c37747a484ed114b1f7e3102ab2a451c6b3
fmohamed <fmohamed@F.M>
2020-02-25 03:03:34 +0200

    WIP: Create datatable

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { YoutubeService } from "src/app/services/youtube.service";
+import { MatTableDataSource } from "@angular/material/table";
+import { Video } from "src/app/models/video";
+import { MatPaginator } from "@angular/material/paginator";
+import { VideosDataSource } from "src/app/data/videos-data-source";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  maxResults = 8;
+  displayedColumns: string[] = ["thumbnail", "title", "publishedAt", "details"];
+  dataSource: VideosDataSource;
 
-  constructor() { }
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor(private youtubeService: YoutubeService) {}
 
   ngOnInit(): void {
+    this.dataSource = new VideosDataSource(this.youtubeService);
+    this.dataSource.loadVideos("UCixD9UbKvDxzGNiPC_fgHyA", this.maxResults);
   }
-
 }
