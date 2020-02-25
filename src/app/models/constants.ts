66dcd180d116fe35c1d2cec8b7c863d1accbfcb2
fmohamed <fmohamed@F.M>
2020-02-25 16:28:52 +0200

    Set global key for local favorites

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -2,3 +2,5 @@ export const YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3";
 export const GET_CHANNEL_VIDEOS_ENDPOINT = "/search?part=snippet&type=video";
 export const GET_VIDEO_ENDPOINT =
   "/videos?part=snippet%2CcontentDetails%2Cstatistics";
+
+export const KEY_FAV = "favorites";
