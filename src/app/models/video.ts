89a8f426d87af995ae0ff64a45413a8a445a0c55
fmohamed <fmohamed@F.M>
2020-02-25 12:10:37 +0200

    Add id to video model

--- src/app/models/video.ts
+++ src/app/models/video.ts
@@ -1,9 +1,11 @@
 export class Video {
+  id;
   title;
   thumbnailUrl;
   publishedAt;
 
   constructor(item: any) {
+    this.id = item.id.videoId;
     this.title = item.snippet.title;
     this.thumbnailUrl = item.snippet.thumbnails.medium.url;
     this.publishedAt = item.snippet.publishedAt;
