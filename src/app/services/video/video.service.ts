8c0c3586422d1e52adc86893f9529c7561a30fb5
fmohamed <fmohamed@F.M>
2020-02-25 15:31:58 +0200

    Create getVideo request

--- src/app/services/video/video.service.ts
+++ src/app/services/video/video.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpParams, HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { YOUTUBE_API_URL, GET_VIDEO_ENDPOINT } from "src/app/models/constants";
+import { map } from "rxjs/operators";
+import { Video } from "src/app/models/video";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class VideoService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getVideo(videoId: string) {
+    let parameters = new HttpParams()
+      .set("id", videoId)
+      .set("key", environment.youtube_api_key);
+
+    return this.http
+      .get(YOUTUBE_API_URL + GET_VIDEO_ENDPOINT, {
+        params: parameters
+      })
+      .pipe(
+        map(res => {
+          return new Video(res["items"][0]);
+        })
+      );
+  }
 }
