e8042084f7c94af46a6153e6e9fdde76978322ea
fmohamed <fmohamed@F.M>
2020-02-25 15:31:45 +0200

    Add maxRes thumbnail, description, likeCount, viewCount & duration to video model

--- src/app/models/video.ts
+++ src/app/models/video.ts
@@ -1,13 +1,23 @@
 export class Video {
   id;
   title;
-  thumbnailUrl;
+  thumbnailMedUrl;
+  thumbnailMaxResUrl;
   publishedAt;
+  description;
+  duration;
+  viewCount;
+  likeCount;
 
   constructor(item: any) {
-    this.id = item.id.videoId;
+    this.id = item.id.videoId ? item.id.videoId : item.id;
     this.title = item.snippet.title;
-    this.thumbnailUrl = item.snippet.thumbnails.medium.url;
+    this.thumbnailMedUrl = item.snippet.thumbnails.medium.url;
+    this.thumbnailMaxResUrl = item.snippet.thumbnails.maxres?.url;
     this.publishedAt = item.snippet.publishedAt;
+    this.description = item.snippet.description;
+    this.viewCount = item.statistics?.viewCount;
+    this.likeCount = item.statistics?.likeCount;
+    this.duration = item.contentDetails?.duration;
   }
 }
