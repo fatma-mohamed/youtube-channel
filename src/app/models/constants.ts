ab9f8b9f7a82052c4294c924e9264cda9fa2ee1b
fmohamed <fmohamed@F.M>
2020-02-25 15:31:16 +0200

    Add "statistics" to VIDEO_ENDPOINT

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,2 +1,4 @@
 export const YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3";
 export const GET_CHANNEL_VIDEOS_ENDPOINT = "/search?part=snippet&type=video";
+export const GET_VIDEO_ENDPOINT =
+  "/videos?part=snippet%2CcontentDetails%2Cstatistics";
